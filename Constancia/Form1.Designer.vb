e31da63dbb01b7a2b975604b0b988adc464374d8
ABluishGuy <74685625+Azulioos@users.noreply.github.com>
2021-12-07 17:17:49 -0600

    Add files via upload

---
+++ Constancia/Form1.Designer.vb
@@ -0,0 +1,323 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label18 = New System.Windows.Forms.Label()
+        Me.Label19 = New System.Windows.Forms.Label()
+        Me.Label20 = New System.Windows.Forms.Label()
+        Me.Label21 = New System.Windows.Forms.Label()
+        Me.Label22 = New System.Windows.Forms.Label()
+        Me.Label23 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label24 = New System.Windows.Forms.Label()
+        Me.Label25 = New System.Windows.Forms.Label()
+        Me.Label26 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 45)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(100, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Nombre del Equipo:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 93)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(106, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Nombre del Usuario: "
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 118)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(96, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Sistema Operativo:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(109, 45)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(40, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Equipo"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(124, 93)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(43, 13)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Usuario"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(114, 118)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(22, 13)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "SO"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(113, 173)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(61, 13)
+        Me.Label7.TabIndex = 7
+        Me.Label7.Text = "Procesador"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(11, 173)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(96, 13)
+        Me.Label8.TabIndex = 6
+        Me.Label8.Text = "Procesador del pc:"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(109, 70)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(47, 13)
+        Me.Label9.TabIndex = 9
+        Me.Label9.Text = "Memoria"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(12, 70)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(96, 13)
+        Me.Label10.TabIndex = 8
+        Me.Label10.Text = "Memoria Instalada:"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(104, 198)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(52, 13)
+        Me.Label11.TabIndex = 11
+        Me.Label11.Text = "Direccion"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(12, 198)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(79, 13)
+        Me.Label12.TabIndex = 10
+        Me.Label12.Text = "Direccion Mac:"
+        '
+        'Label18
+        '
+        Me.Label18.AutoSize = True
+        Me.Label18.Location = New System.Drawing.Point(109, 22)
+        Me.Label18.Name = "Label18"
+        Me.Label18.Size = New System.Drawing.Size(70, 13)
+        Me.Label18.TabIndex = 19
+        Me.Label18.Text = "ID del equipo"
+        '
+        'Label19
+        '
+        Me.Label19.AutoSize = True
+        Me.Label19.Location = New System.Drawing.Point(11, 22)
+        Me.Label19.Name = "Label19"
+        Me.Label19.Size = New System.Drawing.Size(76, 13)
+        Me.Label19.TabIndex = 18
+        Me.Label19.Text = "ID del equipo: "
+        '
+        'Label20
+        '
+        Me.Label20.AutoSize = True
+        Me.Label20.Location = New System.Drawing.Point(209, 70)
+        Me.Label20.Name = "Label20"
+        Me.Label20.Size = New System.Drawing.Size(21, 13)
+        Me.Label20.TabIndex = 20
+        Me.Label20.Text = "Gb"
+        '
+        'Label21
+        '
+        Me.Label21.AutoSize = True
+        Me.Label21.Location = New System.Drawing.Point(12, 356)
+        Me.Label21.Name = "Label21"
+        Me.Label21.Size = New System.Drawing.Size(40, 13)
+        Me.Label21.TabIndex = 21
+        Me.Label21.Text = "CURP:"
+        '
+        'Label22
+        '
+        Me.Label22.AutoSize = True
+        Me.Label22.Location = New System.Drawing.Point(12, 385)
+        Me.Label22.Name = "Label22"
+        Me.Label22.Size = New System.Drawing.Size(47, 13)
+        Me.Label22.TabIndex = 22
+        Me.Label22.Text = "Nombre:"
+        '
+        'Label23
+        '
+        Me.Label23.AutoSize = True
+        Me.Label23.Location = New System.Drawing.Point(12, 321)
+        Me.Label23.Name = "Label23"
+        Me.Label23.Size = New System.Drawing.Size(146, 13)
+        Me.Label23.TabIndex = 23
+        Me.Label23.Text = "Generar una firma electronica"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(68, 349)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(418, 20)
+        Me.TextBox1.TabIndex = 24
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(68, 378)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(418, 20)
+        Me.TextBox2.TabIndex = 25
+        '
+        'Label24
+        '
+        Me.Label24.AutoSize = True
+        Me.Label24.Location = New System.Drawing.Point(12, 144)
+        Me.Label24.Name = "Label24"
+        Me.Label24.Size = New System.Drawing.Size(92, 13)
+        Me.Label24.TabIndex = 26
+        Me.Label24.Text = "Fecha de firmado:"
+        '
+        'Label25
+        '
+        Me.Label25.AutoSize = True
+        Me.Label25.Location = New System.Drawing.Point(346, 153)
+        Me.Label25.Name = "Label25"
+        Me.Label25.Size = New System.Drawing.Size(30, 13)
+        Me.Label25.TabIndex = 27
+        Me.Label25.Text = "Hora"
+        '
+        'Label26
+        '
+        Me.Label26.AutoSize = True
+        Me.Label26.Location = New System.Drawing.Point(109, 144)
+        Me.Label26.Name = "Label26"
+        Me.Label26.Size = New System.Drawing.Size(37, 13)
+        Me.Label26.TabIndex = 28
+        Me.Label26.Text = "Fecha"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(194, 429)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(115, 20)
+        Me.Button2.TabIndex = 29
+        Me.Button2.Text = "Firmar"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(520, 488)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label26)
+        Me.Controls.Add(Me.Label25)
+        Me.Controls.Add(Me.Label24)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label23)
+        Me.Controls.Add(Me.Label22)
+        Me.Controls.Add(Me.Label21)
+        Me.Controls.Add(Me.Label20)
+        Me.Controls.Add(Me.Label18)
+        Me.Controls.Add(Me.Label19)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Constancia de Equipo"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label18 As Label
+    Friend WithEvents Label19 As Label
+    Friend WithEvents Label20 As Label
+    Friend WithEvents Label21 As Label
+    Friend WithEvents Label22 As Label
+    Friend WithEvents Label23 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label24 As Label
+    Friend WithEvents Label25 As Label
+    Friend WithEvents Label26 As Label
+    Friend WithEvents Button2 As Button
+End Class
---
+++ Constancia/Form1.vb
@@ -0,0 +1,416 @@
+﻿Imports System.Management
+Imports System.Text
+Imports iTextSharp.text
+Imports iTextSharp.text.pdf
+
+Public Class Form1
+
+    Dim _software As String = ""
+    Dim _version As String = ""
+    Dim _desarrollador As String = ""
+    Dim _fechadeuso As String = ""
+
+
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+        Dim _nameSpace$ = "root\CIMV2"
+
+        Dim wql = "SELECT * FROM WIN32_PROCESSOR"
+
+        Dim _strbuilder As New StringBuilder
+
+        Using _moSearcher As New ManagementObjectSearcher(_nameSpace, wql)
+
+            For Each _mobject As ManagementObject In _moSearcher.Get
+                Label7.Text = $"{_mobject("Name")}"
+            Next
+
+        End Using
+
+
+        Dim wql2 = "SELECT * FROM WIN32_NetworkAdapter Where AdapterType='Ethernet 802.3'"
+
+        Dim _strbuilder2 As New StringBuilder
+
+        Using _moSearcher2 As New ManagementObjectSearcher(_nameSpace, wql2)
+
+            For Each _mobject2 As ManagementObject In _moSearcher2.Get
+                Label11.Text = $"{_mobject2("MACAddress")}"
+            Next
+
+        End Using
+
+
+        Dim wql4 = "SELECT * FROM WIN32_BIOS"
+
+        Dim _strbuilder4 As New StringBuilder
+
+        Using _moSearcher4 As New ManagementObjectSearcher(_nameSpace, wql4)
+
+            For Each _mobject4 As ManagementObject In _moSearcher4.Get
+                Label18.Text = $"{_mobject4("SerialNumber")}"
+            Next
+
+        End Using
+
+
+
+        Dim memo = My.Computer.Info.TotalPhysicalMemory
+        Dim memo2 = memo * (9.31 * (10 ^ -10))
+
+        Label4.Text = My.Computer.Name
+        Label5.Text = My.User.Name
+        Label6.Text = My.Computer.Info.OSFullName + " Version: " + My.Computer.Info.OSVersion
+        Label9.Text = memo2
+
+        Label25.Text = DateTime.Now.ToString("HH:mm:ss")
+        Label26.Text = DateTime.Now.ToString("dddd,dd,MMMM,yyy")
+
+
+
+
+    End Sub
+
+    Private Sub Label8_Click(sender As Object, e As EventArgs) Handles Label8.Click
+
+    End Sub
+
+    Private Sub Label7_Click(sender As Object, e As EventArgs) Handles Label7.Click
+
+    End Sub
+
+    Private Sub Label10_Click(sender As Object, e As EventArgs) Handles Label10.Click
+
+    End Sub
+
+    Private Sub Label9_Click(sender As Object, e As EventArgs) Handles Label9.Click
+
+    End Sub
+
+    Private Sub Label12_Click(sender As Object, e As EventArgs) Handles Label12.Click
+
+    End Sub
+
+    Private Sub Label11_Click(sender As Object, e As EventArgs) Handles Label11.Click
+
+    End Sub
+
+    Private Sub Label14_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Label13_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+
+
+        Dim _nameSpace$ = "root\CIMV2"
+
+        Dim wql3 = "SELECT * FROM WIN32_Product"
+
+        Dim _strbuilder3 As New StringBuilder
+
+
+        Using _moSearcher3 As New ManagementObjectSearcher(_nameSpace, wql3)
+
+
+            For Each _mobject3 As ManagementObject In _moSearcher3.Get
+                _software = _software + $"{_mobject3("Name")}" & vbCrLf
+                _version = _version + $"{_mobject3("version")}" & vbCrLf
+                _desarrollador = _desarrollador + $"{_mobject3("vendor")}" & vbCrLf
+                _fechadeuso = _fechadeuso + $"{_mobject3("installdate")}" & vbCrLf
+
+
+
+            Next
+
+        End Using
+
+
+
+
+    End Sub
+
+    Private Sub TableLayoutPanel1_Paint(sender As Object, e As PaintEventArgs)
+
+    End Sub
+
+    Private Sub Label19_Click(sender As Object, e As EventArgs) Handles Label19.Click
+
+    End Sub
+
+    Private Sub Label20_Click(sender As Object, e As EventArgs) Handles Label20.Click
+
+    End Sub
+
+    Private Sub Label21_Click(sender As Object, e As EventArgs) Handles Label21.Click
+
+    End Sub
+
+    Private Sub Label22_Click(sender As Object, e As EventArgs) Handles Label22.Click
+
+    End Sub
+
+    Private Sub Label23_Click(sender As Object, e As EventArgs) Handles Label23.Click
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        Dim SaveFileDialog As New SaveFileDialog
+        Dim ruta As String
+        With SaveFileDialog
+            .Title = "Guardar"
+            .InitialDirectory = My.Computer.FileSystem.SpecialDirectories.MyDocuments
+            .Filter = "Archivo pdf (*.pdf)|*.pdf"
+            .FileName = "Proyecto ordinario - Administracion de software"
+            .OverwritePrompt = True
+            .CheckPathExists = True
+        End With
+
+        If SaveFileDialog.ShowDialog() = System.Windows.Forms.DialogResult.OK Then
+            ruta = SaveFileDialog.FileName
+        Else
+            ruta = String.Empty
+            Exit Sub
+        End If
+
+        Try
+            Dim document As New iTextSharp.text.Document(PageSize.LETTER)
+            document.PageSize.Rotate()
+
+            document.AddAuthor(Label1.ToString)
+            document.AddTitle("Crear pdf")
+
+
+            Dim writer As PdfWriter = PdfWriter.GetInstance(document, New System.IO.FileStream _
+            (ruta, System.IO.FileMode.Create))
+            writer.ViewerPreferences = PdfWriter.PageLayoutSinglePage
+
+            document.Open()
+            Dim cb As PdfContentByte = writer.DirectContent
+            Dim bf As BaseFont = BaseFont.CreateFont(BaseFont.TIMES_ROMAN, BaseFont.CP1252, BaseFont.NOT_EMBEDDED)
+
+            cb.SetFontAndSize(bf, 10)
+
+            Dim pdfTable As New PdfPTable(2)
+
+            Dim PdfTitulo As New PdfPCell(New Phrase("Constancia de uso de un ordenador - Proyecto ordinario", New Font(bf, 20, Font.Bold)))
+            PdfTitulo.HorizontalAlignment = iTextSharp.text.Element.ALIGN_CENTER
+            PdfTitulo.Colspan = 4
+            PdfTitulo.Border = 0
+            PdfTitulo.FixedHeight = 60
+            pdfTable.AddCell(PdfTitulo)
+
+
+
+            Dim Table2 As PdfPTable = New PdfPTable(4)
+            Dim Table1 As PdfPTable = New PdfPTable(4)
+            Dim Table3 As PdfPTable = New PdfPTable(4)
+            Dim Table4 As PdfPTable = New PdfPTable(2)
+
+            Dim width1 As Single() = New Single() {1.0F, 1.0F, 1.0F, 1.0F}
+            Dim width2 As Single() = New Single() {2.5F, 0.5F, 1.0F, 0.5F}
+            Dim width3 As Single() = New Single() {2.5F, 0.5F, 1.0F, 0.5F}
+            Dim width4 As Single() = New Single() {1.0F, 1.0F}
+
+            Table1.WidthPercentage = 90
+            Table2.WidthPercentage = 90
+            Table3.WidthPercentage = 90
+            Table4.WidthPercentage = 90
+
+            Dim CVacio As PdfPCell = New PdfPCell(New Phrase(""))
+
+            Dim Columna_1 As PdfPCell
+            Dim Columna_2 As PdfPCell
+            Dim Columna_3 As PdfPCell
+            Dim Columna_4 As PdfPCell
+
+            Dim Columna_5 As PdfPCell
+            Dim Columna_6 As PdfPCell
+            Dim Columna_7 As PdfPCell
+            Dim Columna_8 As PdfPCell
+
+            Dim Columna_9 As PdfPCell
+            Dim Columna_10 As PdfPCell
+            Dim Columna_11 As PdfPCell
+            Dim Columna_12 As PdfPCell
+
+            Dim Columna_13 As PdfPCell
+            Dim Columna_14 As PdfPCell
+
+            CVacio.Border = 0
+            Table1.SetWidths(width1)
+            Table2.SetWidths(width2)
+            Table3.SetWidths(width3)
+            Table4.SetWidths(width4)
+
+            Table1.AddCell(CVacio)
+            Table1.AddCell(CVacio)
+            Table1.AddCell(CVacio)
+            Table1.AddCell(CVacio)
+
+            Table2.AddCell(CVacio)
+            Table2.AddCell(CVacio)
+            Table2.AddCell(CVacio)
+            Table2.AddCell(CVacio)
+
+
+            Table3.AddCell(CVacio)
+            Table3.AddCell(CVacio)
+            Table3.AddCell(CVacio)
+            Table3.AddCell(CVacio)
+
+            Table4.AddCell(CVacio)
+            Table4.AddCell(CVacio)
+
+            Columna_5 = New PdfPCell(New Phrase("ID del equipo: " & Me.Label18.Text, New Font(bf, 6)))
+            Columna_5.Border = 0
+            Table1.AddCell(Columna_5)
+
+            Columna_6 = New PdfPCell(New Phrase("Nombre del equipo: " & Me.Label4.Text, New Font(bf, 6)))
+            Columna_6.Border = 0
+            Table1.AddCell(Columna_6)
+
+            Columna_7 = New PdfPCell(New Phrase("Procesador del equipo: " & Me.Label7.Text, New Font(bf, 6)))
+            Columna_7.Border = 0
+            Table1.AddCell(Columna_7)
+
+            Columna_8 = New PdfPCell(New Phrase("Memoria Instalada: " & Me.Label9.Text + "GB", New Font(bf, 6)))
+            Columna_8.Border = 0
+            Table1.AddCell(Columna_8)
+
+
+
+            Columna_5 = New PdfPCell(New Phrase("Direccion MAC: " & Me.Label11.Text, New Font(bf, 6)))
+            Columna_5.Border = 0
+            Table1.AddCell(Columna_5)
+
+            Columna_6 = New PdfPCell(New Phrase("Nombre del Usuario: " & Me.Label5.Text, New Font(bf, 6)))
+            Columna_6.Border = 0
+            Table1.AddCell(Columna_6)
+
+            Columna_7 = New PdfPCell(New Phrase("Sistema Operativo: " & Me.Label6.Text, New Font(bf, 6)))
+            Columna_7.Border = 0
+            Table1.AddCell(Columna_7)
+
+            Columna_8 = New PdfPCell(New Phrase("-----------------", New Font(bf, 6)))
+            Columna_8.Border = 0
+            Table1.AddCell(Columna_8)
+
+            Table1.AddCell(CVacio)
+            Table1.AddCell(CVacio)
+            Table1.AddCell(CVacio)
+            Table1.AddCell(CVacio)
+
+            Columna_9 = New PdfPCell(New Phrase("Software", New Font(bf, 5)))
+            Columna_9.Border = 0
+            Table3.AddCell(Columna_9)
+
+            Columna_10 = New PdfPCell(New Phrase("Version", New Font(bf, 5)))
+            Columna_10.Border = 0
+            Table3.AddCell(Columna_10)
+
+            Columna_11 = New PdfPCell(New Phrase("Desarrolladores", New Font(bf, 5)))
+            Columna_11.Border = 0
+            Table3.AddCell(Columna_11)
+
+            Columna_12 = New PdfPCell(New Phrase("Fecha de uso", New Font(bf, 5)))
+            Columna_12.Border = 0
+            Table3.AddCell(Columna_12)
+
+
+
+            Dim _nameSpace$ = "root\CIMV2"
+
+            Dim wql3 = "SELECT * FROM WIN32_Product"
+
+            Dim _strbuilder3 As New StringBuilder
+
+            Using _moSearcher3 As New ManagementObjectSearcher(_nameSpace, wql3)
+
+                For Each _mobject3 As ManagementObject In _moSearcher3.Get
+
+
+                    Columna_1 = New PdfPCell(New Phrase($"{_mobject3("Name")}", New Font(bf, 5)))
+                    Columna_1.Border = 0
+                    Table2.AddCell(Columna_1)
+
+                    Columna_2 = New PdfPCell(New Phrase($"{_mobject3("version")}", New Font(bf, 5)))
+                    Columna_2.Border = 0
+                    Table2.AddCell(Columna_2)
+
+                    Columna_3 = New PdfPCell(New Phrase($"{_mobject3("vendor")}", New Font(bf, 5)))
+                    Columna_3.Border = 0
+                    Table2.AddCell(Columna_3)
+
+                    Columna_4 = New PdfPCell(New Phrase($"{_mobject3("installdate")}", New Font(bf, 5)))
+                    Columna_4.Border = 0
+                    Table2.AddCell(Columna_4)
+
+
+
+                Next
+
+            End Using
+
+            Table2.AddCell(CVacio)
+            Table2.AddCell(CVacio)
+            Table2.AddCell(CVacio)
+            Table2.AddCell(CVacio)
+
+            Columna_13 = New PdfPCell(New Phrase("Nombre del firmante: " & Me.TextBox2.Text, New Font(bf, 10)))
+            Columna_13.Border = 0
+            Table4.AddCell(Columna_13)
+
+            Columna_14 = New PdfPCell(New Phrase("CURP: " & Me.TextBox1.Text, New Font(bf, 10)))
+            Columna_14.Border = 0
+            Table4.AddCell(Columna_14)
+
+            Columna_13 = New PdfPCell(New Phrase("Fecha y hora de firmado: " & Me.Label26.Text, New Font(bf, 10)))
+            Columna_13.Border = 0
+            Table4.AddCell(Columna_13)
+
+            Columna_14 = New PdfPCell(New Phrase(Me.Label25.Text, New Font(bf, 10)))
+            Columna_14.Border = 0
+            Table4.AddCell(Columna_14)
+
+            document.Add(pdfTable)
+            document.Add(Table1)
+            document.Add(Table3)
+            document.Add(Table2)
+            document.Add(Table4)
+
+            document.Close()
+
+        Catch ex As Exception
+            MessageBox.Show("Error de la generacion", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+
+        End Try
+
+    End Sub
+
+    Private Sub Label17_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Label16_Click(sender As Object, e As EventArgs)
+
+    End Sub
+End Class
